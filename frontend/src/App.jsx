1b33293edbdf2d6e89dbb2c7343b7ea9f24285cc
Mehfooz Kudchikar <mehfoozkudchikar@gmail.com>
2025-10-16 21:51:35 +0530

    protected routes resolve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,76 +1,88 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom'; // Removed BrowserRouter and useLocation
-import Navbar from './components/navbar';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import FounderDetails from './pages/FounderDetails';
-import InvesterDetails from './pages/InvesterDetails';
-import FounderProfile from './pages/FounderProfile';
-import InvestorProfile from './pages/InvestorProfile';
-import ExploreStartups from './pages/ExploreStartups';
-import UpdateStartup from './pages/UpdateStartup';
-import StartupDetail from './pages/StartupDetail';
-import ChatPage from './pages/ChatPage';
-import FounderDMPage from './pages/FounderDMPage';
-import AddStartup from './pages/AddStartup';
-import AboutUs from './pages/AboutUs';
-import ProtectedRoute from './Components/ProtectedRoute';
-import ChatBotWidget from './components/ChatBotWidget';
+// src/App.jsx
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import FounderDetails from "./pages/FounderDetails";
+import InvesterDetails from "./pages/InvesterDetails";
+import FounderProfile from "./pages/FounderProfile";
+import InvestorProfile from "./pages/InvestorProfile";
+import ExploreStartups from "./pages/ExploreStartups";
+import UpdateStartup from "./pages/UpdateStartup";
+import StartupDetail from "./pages/StartupDetail";
+import ChatPage from "./pages/ChatPage";
+import FounderDMPage from "./pages/FounderDMPage";
+import AddStartup from "./pages/AddStartup";
+import AboutUs from "./pages/AboutUs";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ChatBotWidget from "./components/ChatBotWidget";
 
 function App() {
   return (
-    <div>
-      {/* Navbar */}
-      <Navbar />
+    <BrowserRouter>
+      <div>
+        {/* Navbar */}
+        <Navbar />
 
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup-founder" element={<FounderDetails />} />
-        <Route path="/signup-investor" element={<InvesterDetails />} />
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup-founder" element={<FounderDetails />} />
+          <Route path="/signup-investor" element={<InvesterDetails />} />
 
-        {/* Protected Routes */}
-        <Route
-          path="/founder-profile"
-          element={
-              <FounderProfile />
-          }
-        />
-        <Route
-          path="/investor-profile"
-          element={
-              <InvestorProfile />
-          }
-        />
-        <Route
-          path="/add-startup"
-          element={
-              <AddStartup />
-          }
-        />
-        <Route
-          path="/update-startup/:startupId"
-          element={
-              <UpdateStartup />
-          }
-        />
-        <Route path="/founder-dm" element={<FounderDMPage />} />
-        <Route path="/chat/:founderId" element={<ChatPage />} />
-        <Route path="/startup/:startupId" element={<StartupDetail />} />
-        <Route path="/explore-startups" element={<ExploreStartups />} />
-        <Route path="/about-us" element={<AboutUs />} />
-      </Routes>
+          {/* Protected Routes */}
+          <Route
+            path="/founder-profile"
+            element={
+              <ProtectedRoute>
+                <FounderProfile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/investor-profile"
+            element={
+              <ProtectedRoute>
+                <InvestorProfile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/add-startup"
+            element={
+              <ProtectedRoute>
+                <AddStartup />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/update-startup/:startupId"
+            element={
+              <ProtectedRoute>
+                <UpdateStartup />
+              </ProtectedRoute>
+            }
+          />
 
-      {/* ChatBot Widget */}
-      <ChatBotWidget />
+          <Route path="/founder-dm" element={<FounderDMPage />} />
+          <Route path="/chat/:founderId" element={<ChatPage />} />
+          <Route path="/startup/:startupId" element={<StartupDetail />} />
+          <Route path="/explore-startups" element={<ExploreStartups />} />
+          <Route path="/about-us" element={<AboutUs />} />
+        </Routes>
 
-      {/* Footer */}
-      <Footer />
-    </div>
+        {/* ChatBot Widget */}
+        <ChatBotWidget />
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
